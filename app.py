6e76b758498479b07d87eb2389269c1605581f4e
Ali-Hassan333 <dev.hassan33@gmail.com>
2024-05-13 18:14:21 +0500

    first commit

---
+++ app.py
@@ -0,0 +1,199 @@
+import os
+import openai
+import streamlit as st
+from dotenv import load_dotenv
+import param
+import requests
+from langchain_core.pydantic_v1 import BaseModel, Field
+import datetime
+from langchain_core.tools import tool   
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.utils.function_calling import convert_to_openai_function
+from langchain.agents.format_scratchpad import format_to_openai_functions
+from langchain.agents.output_parsers import OpenAIFunctionsAgentOutputParser
+
+
+load_dotenv()
+
+openai.api_key = st.text_input("Enter Your OpenAI API Key:",type="password")
+
+
+
+
+# Define the input schema
+class OpenMeteoInput(BaseModel):
+    latitude: float = Field(..., description="Latitude of the location to fetch weather data for")
+    longitude: float = Field(..., description="Longitude of the location to fetch weather data for")
+
+@tool(args_schema=OpenMeteoInput)
+def get_current_temperature(latitude: float, longitude: float) -> dict:
+    """Fetch current temperature for given coordinates."""
+    
+    BASE_URL = "https://api.open-meteo.com/v1/forecast"
+    
+    # Parameters for the request
+    params = {
+        'latitude': latitude,
+        'longitude': longitude,
+        'hourly': 'temperature_2m',
+        'forecast_days': 1,
+    }
+
+    # Make the request
+    response = requests.get(BASE_URL, params=params)
+    
+    if response.status_code == 200:
+        results = response.json()
+    else:
+        raise Exception(f"API Request failed with status code: {response.status_code}")
+
+    current_utc_time = datetime.datetime.now()
+    time_list = [datetime.datetime.fromisoformat(time_str.replace('Z', '+00:00')) for time_str in results['hourly']['time']]
+    temperature_list = results['hourly']['temperature_2m']
+    
+    closest_time_index = min(range(len(time_list)), key=lambda i: abs(time_list[i] - current_utc_time))
+    current_temperature = temperature_list[closest_time_index]
+    
+    return f'The current temperature is {current_temperature}°C'
+
+
+import wikipedia
+
+class SearchWikipediaInput(BaseModel):
+    query: str = Field(..., description="Query to search Wikipedia for")
+
+@tool(args_schema=SearchWikipediaInput)
+def search_wikipedia(query: str) -> str:
+    """Run Wikipedia search and get page summaries."""
+    page_titles = wikipedia.search(query)
+    summaries = []
+    for page_title in page_titles[: 3]:
+        try:
+            wiki_page =  wikipedia.page(title=page_title, auto_suggest=False)
+            summaries.append(f"Page: {page_title}\nSummary: {wiki_page.summary}")
+        except (
+            
+        ):
+            pass
+    if not summaries:
+        return "No good Wikipedia Search Result was found"
+    return "\n\n".join(summaries)
+
+tools = [get_current_temperature, search_wikipedia]
+
+functions = [convert_to_openai_function(f) for f in tools]
+model = ChatOpenAI(temperature=0).bind(functions=functions)
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are helpful but sassy assistant"),
+    MessagesPlaceholder(variable_name="chat_history"),
+    ("user", "{input}"),
+    MessagesPlaceholder(variable_name="agent_scratchpad")
+])
+chain = prompt | model | OpenAIFunctionsAgentOutputParser()
+
+
+
+from langchain.schema.agent import AgentFinish
+def run_agent(user_input):
+    intermediate_steps = []
+    while True:
+        result = chain.invoke({
+            "input": user_input, 
+            "agent_scratchpad": format_to_openai_functions(intermediate_steps)
+        })
+        if isinstance(result, AgentFinish):
+            return result
+        tool = {
+            "search_wikipedia": search_wikipedia, 
+            "get_current_temperature": get_current_temperature,
+        }[result.tool]
+        observation = tool.run(result.tool_input)
+        intermediate_steps.append((result, observation))
+
+from langchain.schema.runnable import RunnablePassthrough
+agent_chain = RunnablePassthrough.assign(
+    agent_scratchpad= lambda x: format_to_openai_functions(x["intermediate_steps"])
+) | prompt | model | OpenAIFunctionsAgentOutputParser()
+
+def run_agent(user_input):
+    intermediate_steps = []
+    while True:
+        result = agent_chain.invoke({
+            "input": user_input, 
+            "intermediate_steps": intermediate_steps
+        })
+        if isinstance(result, AgentFinish):
+            return result
+        tool = {
+            "search_wikipedia": search_wikipedia, 
+            "get_current_temperature": get_current_temperature,
+        }[result.tool]
+        observation = tool.run(result.tool_input)
+        intermediate_steps.append((result, observation))
+
+
+from langchain.memory import ConversationBufferMemory
+memory = ConversationBufferMemory(return_messages=True,memory_key="chat_history")        
+
+
+from langchain.agents import AgentExecutor
+agent_executor = AgentExecutor(agent=agent_chain, tools=tools, verbose=True, memory=memory)        
+
+
+   
+
+
+
+
+
+def display_user_message(message):
+    st.markdown(f'<div style="background-color: #262730; border-radius: 10px; padding: 10px; margin: 10px 0; text-align: left;">{message}</div>', unsafe_allow_html=True)
+
+def display_bot_message(message):
+    st.markdown(f'<div style="background-color: #262730; color: white; border-radius: 10px; padding: 10px; margin: 10px 0; text-align: left;">{message}</div>', unsafe_allow_html=True)
+
+class cbfs:
+    def __init__(self, tools):
+        self.functions = [convert_to_openai_function(f) for f in tools]
+        self.model = ChatOpenAI(temperature=0).bind(functions=self.functions)
+        self.memory = ConversationBufferMemory(return_messages=True, memory_key="chat_history")
+        self.prompt = ChatPromptTemplate.from_messages([
+            ("system", "You are helpful but sassy assistant"),
+            MessagesPlaceholder(variable_name="chat_history"),
+            ("user", "{input}"),
+            MessagesPlaceholder(variable_name="agent_scratchpad")
+        ])
+        self.chain = RunnablePassthrough.assign(
+            agent_scratchpad=lambda x: format_to_openai_functions(x["intermediate_steps"])
+        ) | self.prompt | self.model | OpenAIFunctionsAgentOutputParser()
+        self.qa = AgentExecutor(agent=self.chain, tools=tools, verbose=False, memory=self.memory)
+
+    def convchain(self, query):
+        if not query:
+            return
+        result = self.qa.invoke({"input": query})
+        self.answer = result['output']
+        return f"🙎‍♂️:  {query}", f"🤖:  {self.answer}"
+
+    def clr_history(self):
+        pass
+
+if 'chat_history' not in st.session_state:
+    st.session_state['chat_history'] = []
+
+cb = cbfs(tools)
+
+query = st.text_input('Enter text here…')
+if st.button('Send'):
+    user_msg, bot_msg = cb.convchain(query)
+    st.session_state['chat_history'].append((user_msg, bot_msg))
+    st.rerun()
+
+# if st.button('Clear History'):
+#     st.session_state['chat_history'] = []
+#     st.rerun()
+
+for user_msg, bot_msg in st.session_state['chat_history']:
+    display_user_message(user_msg)
+    display_bot_message(bot_msg)
